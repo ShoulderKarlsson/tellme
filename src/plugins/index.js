cdeed1aba73438197f7ebb8f604938068efd9cb2
Axel Karlsson <shoulderkarlsson@gmail.com>
2018-05-06 07:56:34 +0200

    fix: adding network-plugin to exported plugins

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,5 +1,7 @@
-import {weekPlugin as week} from './week'
+import { weekPlugin as week } from './week'
+import { networkPlugin as network } from './network'
 
 export default [
-  week
+  week,
+  network
 ]
