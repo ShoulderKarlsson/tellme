ffff61937d385c53c148edd4a4d33e204e804c7f
Axel Karlsson <shoulderkarlsson@gmail.com>
2018-05-19 17:33:19 +0200

    fix: removed dist

--- dist/lib/plugins/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const week_1 = require("./week");
-const network_1 = require("./network");
-const leap_year_1 = require("./leap-year");
-const red_days_1 = require("./red-days");
-exports.plugins = [week_1.weekPlugin, network_1.networkPlugin, leap_year_1.leapYearPlugin, red_days_1.redDaysPlugin];
