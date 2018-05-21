ed7c1f04235c69041c2fc95631c541a73801ed11
Axel Karlsson <shoulderkarlsson@gmail.com>
2018-05-21 05:58:45 +0200

    fix: using ISOWeek instead of week

--- lib/plugins/week.ts
+++ lib/plugins/week.ts
@@ -1,5 +1,5 @@
 import chalk from 'chalk'
-import {getISOWeek, startOfWeek, eachDay, endOfISOWeek, format} from 'date-fns'
+import {getISOWeek, startOfISOWeek, eachDay, endOfISOWeek, format, isToday} from 'date-fns'
 
 export const weekPlugin = {
   description: 'Display week current weeknumber with additional information',
@@ -12,7 +12,7 @@ export const weekPlugin = {
   resolver: () => {
     const d = new Date()
     const currentWeek = getISOWeek(d)
-    const startDateOfWeek = startOfWeek(d)
+    const startDateOfWeek = startOfISOWeek(d)
     const endDateOfWeek = endOfISOWeek(startDateOfWeek)
     const weekDates = eachDay(startDateOfWeek, endDateOfWeek)
 
