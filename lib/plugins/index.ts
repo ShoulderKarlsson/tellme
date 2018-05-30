f0ad66e47c2c1b0149560ca33e62ed41d7cacee5
Axel Karlsson <shoulderkarlsson@gmail.com>
2018-05-30 21:11:09 +0200

    chore: adding year progress plugin to program

--- lib/plugins/index.ts
+++ lib/plugins/index.ts
@@ -2,6 +2,8 @@ import {weekPlugin as week} from './week'
 import {networkPlugin as network} from './network'
 import {leapYearPlugin as leapYear} from './leap-year'
 import {redDaysPlugin as redDays} from './red-days'
+import {yearProgressionPlugin as yearProgression} from './year-progress'
 import {Plugin} from '../entities'
 
-export const plugins: Array<Plugin> = [week, network, leapYear, redDays]
+export const plugins: Array<Plugin> = [week, network, leapYear, redDays, yearProgression
+]
