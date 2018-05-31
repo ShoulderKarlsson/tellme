9ae72c4c3d746556314db8ecb780bcfab0565abc
Axel Karlsson <shoulderkarlsson@gmail.com>
2018-05-31 20:09:22 +0200

    fix: removed year progress plugin

--- lib/plugins/index.ts
+++ lib/plugins/index.ts
@@ -2,8 +2,6 @@ import {weekPlugin as week} from './week'
 import {networkPlugin as network} from './network'
 import {leapYearPlugin as leapYear} from './leap-year'
 import {redDaysPlugin as redDays} from './red-days'
-import {yearProgressionPlugin as yearProgression} from './year-progress'
 import {Plugin} from '../entities'
 
-export const plugins: Array<Plugin> = [week, network, leapYear, redDays, yearProgression
-]
+export const plugins: Array<Plugin> = [week, network, leapYear, redDays]
